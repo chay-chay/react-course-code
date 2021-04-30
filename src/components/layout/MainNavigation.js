d061183c07b2883a678f3f8add56821e3aef931d
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-04-30 17:35:14 -0400

    added custom CSS of MainNavigation page

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,13 +1,20 @@
 import { Link } from 'react-router-dom';
+import NavigationStyles from './MainNavigation.module.css'
 
 function MainNavigation() {
   return (
-    <header>
-      <div>React Meetups</div>
+    <header className={NavigationStyles.header}>
+      <div className={NavigationStyles.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
-          <Link>Some Link</Link>
+          <Link to='/'>Some Link</Link>
+          </li>
+          <li>
+          <Link to='/new-meetup'>Add New Meetup</Link>
+          </li>
+          <li>
+          <Link to='/favorites'>MyFavorites</Link>
           </li>
         </ul>
       </nav>
