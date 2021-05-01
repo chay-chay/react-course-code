9094ea418394f0811dc0b708fd6e8c66c486859b
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-01 12:13:40 -0400

    added custom classname of MeetupItem

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,15 +1,17 @@
+import meetupitem from './MeetupItem.module.css';
+
 function MeetupItem(props) {
   return (
-    <li>
-      <div>
+    <li className={meetupitem.item}>
+      <div className={meetupitem.image}>
         <img src={props.image} alt="" />
       </div>
-      <div>
+      <div className={meetupitem.content}>
         <h3>{props.title}</h3>
         <address>{props.address}</address>
         <p>{props.description}</p>
       </div>
-      <div>
+      <div className={meetupitem.actions}>
         <button>To Favorite</button>
       </div>
     </li>
