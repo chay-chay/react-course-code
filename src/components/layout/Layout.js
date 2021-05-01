b81d0e5a960db288c513d0a5adf225ac54b928fc
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-01 14:14:36 -0400

    created wrapper for layout in app

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,12 @@
+import layout from "./Layout.module.css";
+import MainNavigation from './MainNavigation';
+
+function Layout(props) {
+  return (
+    <div>
+      <MainNavigation />
+      <main className={layout.main}>{props.children}</main>
+    </div>
+  );
+}
+export default Layout;
