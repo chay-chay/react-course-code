0b1dd9d01866eb523e28be65cdcb335a360fcd75
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-03 14:43:34 -0400

    Navigating Programmatically (history.replace)

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,17 +1,22 @@
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://react-getting-started-24005-default-rtdb.firebaseio.com/meetups.json",
       {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify(meetupData),
         headers: {
-          'Content-Type': 'application/json'
-        }
+          "Content-Type": "application/json",
+        },
       }
-    );
+    ).then(() => {
+      history.replace("/");
+    });
   }
 
   return (
