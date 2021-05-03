d01a8b0f00fc70c13a5d29a93a9c7d8903935070
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-03 12:09:28 -0400

    Working with Forms

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,4 +1,30 @@
+import Card from "../ui/Card";
+import newmeetupform from "./NewMeetupForm.module.css";
+
 function NewMeetupForm() {
-  return 
+  return (
+    <Card>
+      <form className={newmeetupform.form}>
+        <div className={newmeetupform.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" />
+        </div>
+        <div className={newmeetupform.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" />
+        </div>
+        <div className={newmeetupform.control}>
+          <label htmlFor="address">Meetup Address</label>
+          <input type="text" required id="address" />
+        </div>
+        <div className={newmeetupform.control}>
+          <label htmlFor="description">Description</label>
+          <textarea id="description" required rows="5"></textarea>
+        </div>
+        <div className={newmeetupform.actions}></div>
+        <button>Add Meetup</button>
+      </form>
+    </Card>
+  );
 }
 export default NewMeetupForm;
